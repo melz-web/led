55ae6883f4d03839c762084de07cc28fe00530bd
benmelz <ben@melz.me>
2022-06-20 13:13:46 -0400

    feat: bootstrap

--- app/client/App.jsx
+++ app/client/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import OnSwitch from './OnSwitch.jsx';
 
-const App = () => (<OnSwitch />);
+const App = () => (
+  <div className={'w-100 h-100 d-flex justify-content-center align-items-center'}>
+    <OnSwitch />
+  </div>
+);
 
 export default App;
