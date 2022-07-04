84629a8f6e7f3792fc72cc88a24364da7119a1a2
benmelz <ben@melz.me>
2022-07-04 12:59:34 -0400

    feat: auto-reconnect

--- app/client/remote.js
+++ app/client/remote.js
@@ -1,24 +1,29 @@
 import { useCallback, useEffect, useState } from 'react';
 
-const url = new URL(location.href);
-url.protocol = 'wss';
-url.pathname = 'state';
-
 const remote = new EventTarget();
 remote.state = { on: false, hue: 0, saturation: 0, value: 255 };
-remote.socket = new WebSocket(url);
 
-remote.socket.addEventListener('message', (message) => {
-  remote.state = JSON.parse(message.data);
-  remote.dispatchEvent(new Event('update'));
-});
+const url = new URL(location.href);
+url.protocol = (process.env['NODE_ENV'] === 'production') ? 'wss' : 'ws';
+url.pathname = 'state';
+
+let socket;
+
+(function connect() {
+  socket = new WebSocket(url);
+  socket.onmessage = (message) => {
+    remote.state = JSON.parse(message.data);
+    remote.dispatchEvent(new Event('update'));
+  };
+  socket.onclose = () => setTimeout(connect, 1000);
+})();
 
 function useRemote(attribute) {
   const [value, overrideValue] = useState(remote.state[attribute]);
 
   const setValue = useCallback((value) => {
     overrideValue(value);
-    remote.socket.send(JSON.stringify({ [attribute]: value }));
+    socket?.send(JSON.stringify({ [attribute]: value }));
   }, []);
 
   const handleMessage = useCallback(() => {
